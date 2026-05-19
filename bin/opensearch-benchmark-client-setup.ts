fd7b5e450a8ca4a327b2836551ca0f3bb37a6045
Tejas Shah <shatejas@amazon.com>
2026-05-18 18:08:54 -0700

    feat: Add CDK stack for OSB client EC2 infrastructure
    SSM-only access with cfn-init validation. Installs Python 3.11,
    opensearch-benchmark, pandas, and h5py via yum/pip. Creates security
    group with ingress to cluster on port 9200.

--- bin/opensearch-benchmark-client-setup.ts
+++ bin/opensearch-benchmark-client-setup.ts
@@ -3,18 +3,24 @@ import * as cdk from 'aws-cdk-lib/core';
 import { OpensearchBenchmarkClientSetupStack } from '../lib/opensearch-benchmark-client-setup-stack';
 
 const app = new cdk.App();
-new OpensearchBenchmarkClientSetupStack(app, 'OpensearchBenchmarkClientSetupStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const vpcId = app.node.tryGetContext('vpcId');
+const clusterSecurityGroupId = app.node.tryGetContext('clusterSecurityGroupId');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+if (!vpcId || !clusterSecurityGroupId) {
+  throw new Error('Required context: -c vpcId=<vpc-id> -c clusterSecurityGroupId=<sg-id>');
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new OpensearchBenchmarkClientSetupStack(app, 'OsbClientStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  vpcId,
+  clusterSecurityGroupId,
+  instanceType: app.node.tryGetContext('instanceType'),
+  clientName: app.node.tryGetContext('clientName'),
+  ebsVolumeSize: app.node.tryGetContext('ebsVolumeSize')
+    ? Number(app.node.tryGetContext('ebsVolumeSize'))
+    : undefined,
 });
--- lib/opensearch-benchmark-client-setup-stack.ts
+++ lib/opensearch-benchmark-client-setup-stack.ts
@@ -1,16 +1,74 @@
 import * as cdk from 'aws-cdk-lib/core';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export interface OsbClientStackProps extends cdk.StackProps {
+  vpcId: string;
+  clusterSecurityGroupId: string;
+  instanceType?: string;
+  clientName?: string;
+  ebsVolumeSize?: number;
+}
 
 export class OpensearchBenchmarkClientSetupStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: OsbClientStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const instanceType = new ec2.InstanceType(props.instanceType ?? 'c6g.4xlarge');
+    const clientName = props.clientName ?? 'osb-client';
+    const ebsSize = props.ebsVolumeSize ?? 100;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'OpensearchBenchmarkClientSetupQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = ec2.Vpc.fromLookup(this, 'ClusterVpc', { vpcId: props.vpcId });
+
+    const clusterSg = ec2.SecurityGroup.fromSecurityGroupId(
+      this, 'ClusterSg', props.clusterSecurityGroupId
+    );
+
+    const osbSg = new ec2.SecurityGroup(this, 'OsbClientSg', {
+      vpc,
+      description: 'OSB client security group',
+      allowAllOutbound: true,
+    });
+
+    const isArm = instanceType.architecture === ec2.InstanceArchitecture.ARM_64;
+    const machineImage = ec2.MachineImage.latestAmazonLinux2023({
+      cpuType: isArm ? ec2.AmazonLinuxCpuType.ARM_64 : ec2.AmazonLinuxCpuType.X86_64,
+    });
+
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      'set -xe',
+      'yum update -y',
+      'yum install python3.11 python3.11-pip python3.11-devel gcc-c++ tmux git -y',
+      'pip3.11 install opensearch-benchmark pandas h5py',
+      'opensearch-benchmark list workloads',
+    );
+
+    const instance = new ec2.Instance(this, 'OsbClientInstance', {
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      instanceType,
+      machineImage,
+      securityGroup: osbSg,
+      userData,
+      blockDevices: [{
+        deviceName: '/dev/xvda',
+        volume: ec2.BlockDeviceVolume.ebs(ebsSize, { volumeType: ec2.EbsDeviceVolumeType.GP3 }),
+      }],
+      init: ec2.CloudFormationInit.fromElements(),
+      initOptions: {
+        timeout: cdk.Duration.minutes(15),
+      },
+    });
+    instance.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+    );
+    cdk.Tags.of(instance).add('Name', clientName);
+
+    clusterSg.addIngressRule(osbSg, ec2.Port.tcp(9200), 'OSB client access to OpenSearch');
+
+    new cdk.CfnOutput(this, 'InstanceId', { value: instance.instanceId });
+    new cdk.CfnOutput(this, 'PrivateIp', { value: instance.instancePrivateIp });
   }
 }
